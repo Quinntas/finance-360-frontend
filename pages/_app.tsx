afcca8a7f0c51d00e1fb58cf36240fb488d56710
Caio Quintas <caioquintassantiago@gmail.com>
2023-06-06 12:51:55 -0300

    add google adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,49 @@
-import { AppProps } from 'next/app'
 import '../styles/index.css'
+import Head from 'next/head'
+import {useRouter} from 'next/router'
+import Script from 'next/script'
+import {useEffect} from 'react'
+import * as gtag from '../lib/gtag'
+import {AppProps} from "next/app";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({Component, pageProps}: AppProps) {
+    const router = useRouter()
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url)
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
+    return (
+        <>
+            <Head>
+                <script async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4195750374838171"
+                        crossOrigin="anonymous"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+                    }}
+                />
+            </Head>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Component {...pageProps} />
+        </>
+    )
 }
