00ba720d6c01858e21054863a54fd006140e71e5
Caio Quintas <caioquintassantiago@gmail.com>
2023-06-06 12:23:51 -0300

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,46 +1,61 @@
 import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import {HOME_OG_IMAGE_URL} from '../lib/constants'
 
 const Meta = () => {
-  return (
-    <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta name="keywords" content="finance 360, financias, economia, estado"/>
-      <meta name="robots" content="index, follow"/>
-      <meta
-        name="description"
-        content={`Finanças 360, o seu guia completo para o mundo das finanças! Aqui, você encontrará informações valiosas e perspicazes sobre todos os aspectos financeiros que afetam a sua vida. Desde dicas de investimento até estratégias para economizar dinheiro, nosso objetivo é fornecer uma visão abrangente e atualizada sobre o universo financeiro.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
-    </Head>
-  )
+    return (
+        <Head>
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/favicon/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/favicon/site.webmanifest"/>
+            <link
+                rel="mask-icon"
+                href="/favicon/safari-pinned-tab.svg"
+                color="#000000"
+            />
+            <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=2E5XW98L7J"
+            />
+
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '2E5XW98L7J');
+        `,
+                }}
+            />
+            <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+            <meta name="msapplication-TileColor" content="#000000"/>
+            <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
+            <meta name="theme-color" content="#000"/>
+            <link rel="alternate" type="application/rss+xml" href="/feed.xml"/>
+            <meta name="keywords" content="finance 360, financias, economia, estado"/>
+            <meta name="robots" content="index, follow"/>
+            <meta
+                name="description"
+                content={`Finanças 360, o seu guia completo para o mundo das finanças! Aqui, você encontrará informações valiosas e perspicazes sobre todos os aspectos financeiros que afetam a sua vida. Desde dicas de investimento até estratégias para economizar dinheiro, nosso objetivo é fornecer uma visão abrangente e atualizada sobre o universo financeiro.`}
+            />
+            <meta property="og:image" content={HOME_OG_IMAGE_URL}/>
+        </Head>
+    )
 }
 
 export default Meta
